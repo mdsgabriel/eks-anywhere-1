bc32b2f205e35c95bfccd7e2198c9935408502ac
Gabriel Mendes da Silva <sigb@amazon.com>
2022-07-05 18:39:59 -0700

    New cilium reconciler

--- controllers/controllers/clusters/vsphere.go
+++ controllers/controllers/clusters/vsphere.go
@@ -2,12 +2,15 @@ package clusters
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"time"
 
 	"github.com/go-logr/logr"
-	apiv1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/cluster-api/controllers/remote"
@@ -17,7 +20,7 @@ import (
 
 	"github.com/aws/eks-anywhere/controllers/controllers/reconciler"
 	anywherev1 "github.com/aws/eks-anywhere/pkg/api/v1alpha1"
-	c "github.com/aws/eks-anywhere/pkg/cluster"
+	eksacluster "github.com/aws/eks-anywhere/pkg/cluster"
 	"github.com/aws/eks-anywhere/pkg/constants"
 	"github.com/aws/eks-anywhere/pkg/executables"
 	"github.com/aws/eks-anywhere/pkg/networking/cilium"
@@ -68,8 +71,8 @@ func NewVSphereReconciler(client client.Client, log logr.Logger, validator *vsph
 	}
 }
 
-func VsphereCredentials(ctx context.Context, cli client.Client) (*apiv1.Secret, error) {
-	secret := &apiv1.Secret{}
+func VsphereCredentials(ctx context.Context, cli client.Client) (*corev1.Secret, error) {
+	secret := &corev1.Secret{}
 	secretKey := client.ObjectKey{
 		Namespace: "eksa-system",
 		Name:      vsphere.CredentialsObjectName,
@@ -115,11 +118,13 @@ func (v *VSphereClusterReconciler) bundles(ctx context.Context, name, namespace 
 	return clusterBundle, nil
 }
 
-func (v *VSphereClusterReconciler) FetchAppliedSpec(ctx context.Context, cs *anywherev1.Cluster) (*c.Spec, error) {
-	return c.BuildSpecForCluster(ctx, cs, v.bundles, v.eksdRelease, nil, nil, nil)
+func (v *VSphereClusterReconciler) FetchAppliedSpec(ctx context.Context, cs *anywherev1.Cluster) (*eksacluster.Spec, error) {
+	return eksacluster.BuildSpecForCluster(ctx, cs, v.bundles, v.eksdRelease, nil, nil, nil)
 }
 
 func (v *VSphereClusterReconciler) Reconcile(ctx context.Context, cluster *anywherev1.Cluster) (reconciler.Result, error) {
+	v.Log.Info("Inside vsphere reconciler")
+
 	dataCenterConfig := &anywherev1.VSphereDatacenterConfig{}
 	dataCenterName := types.NamespacedName{Namespace: cluster.Namespace, Name: cluster.Spec.DatacenterRef.Name}
 	if err := v.Client.Get(ctx, dataCenterName, dataCenterConfig); err != nil {
@@ -152,11 +157,11 @@ func (v *VSphereClusterReconciler) Reconcile(ctx context.Context, cluster *anywh
 	}
 
 	v.Log.V(4).Info("Fetching bundle", "cluster name", cluster.Spec.ManagementCluster.Name)
-	bundles, err := v.bundles(ctx, cluster.Spec.ManagementCluster.Name, "default")
+	bundles, err := v.bundles(ctx, "bundles-1", "eksa-system")
 	if err != nil {
 		return reconciler.Result{}, err
 	}
-	versionsBundle, err := c.GetVersionsBundle(cluster, bundles)
+	versionsBundle, err := eksacluster.GetVersionsBundle(cluster, bundles)
 	if err != nil {
 		return reconciler.Result{}, err
 	}
@@ -166,7 +171,7 @@ func (v *VSphereClusterReconciler) Reconcile(ctx context.Context, cluster *anywh
 		return reconciler.Result{}, err
 	}
 
-	specWithBundles, err := c.BuildSpecFromBundles(cluster, bundles, c.WithEksdRelease(eksd))
+	specWithBundles, err := eksacluster.BuildSpecFromBundles(cluster, bundles, eksacluster.WithEksdRelease(eksd))
 
 	vsphereClusterSpec := vsphere.NewSpec(specWithBundles, machineConfigMap, dataCenterConfig)
 
@@ -253,7 +258,7 @@ func (v *VSphereClusterReconciler) Reconcile(ctx context.Context, cluster *anywh
 	return reconciler.Result{}, nil
 }
 
-func (v *VSphereClusterReconciler) reconcileCNI(ctx context.Context, cluster *anywherev1.Cluster, capiCluster *clusterv1.Cluster, specWithBundles *c.Spec) (reconciler.Result, error) {
+func (v *VSphereClusterReconciler) reconcileCNI(ctx context.Context, cluster *anywherev1.Cluster, capiCluster *clusterv1.Cluster, specWithBundles *eksacluster.Spec) (reconciler.Result, error) {
 	if !conditions.Has(cluster, cniSpecAppliedCondition) || conditions.IsFalse(capiCluster, cniSpecAppliedCondition) {
 		v.Log.Info("Getting remote client", "client for cluster", capiCluster.Name)
 		key := client.ObjectKey{
@@ -265,10 +270,106 @@ func (v *VSphereClusterReconciler) reconcileCNI(ctx context.Context, cluster *an
 			return reconciler.Result{}, err
 		}
 
+		//install cilium if needed
+		// wait for preflight
+		ciliumDS := &v1.DaemonSet{}
+		ciliumDSName := types.NamespacedName{Namespace: "kube-system", Name: "cilium"}
+		err = remoteClient.Get(ctx, ciliumDSName, ciliumDS)
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				v.Log.Info("Cilium DS not found, installing it")
+				helm := executables.NewHelm(executables.NewExecutable("helm"), executables.WithInsecure())
+
+				ci := cilium.NewCilium(nil, helm)
+
+				ciliumSpec, err := ci.GenerateManifest(ctx, specWithBundles, []string{constants.CapvSystemNamespace})
+				if err != nil {
+					return reconciler.Result{}, err
+				}
+				if err := reconciler.ReconcileYaml(ctx, remoteClient, ciliumSpec); err != nil {
+					return reconciler.Result{}, err
+				}
+				return reconciler.Result{}, err
+			}
+
+			return reconciler.Result{}, err
+		}
+
+		//upgrade cilium
+		v.Log.Info("Upgrading Cilium!!!")
+		needsUpgrade, err := ciliumNeedsUpgrade(ctx, v.Log, remoteClient, specWithBundles)
+		if err != nil {
+			return reconciler.Result{}, err
+		}
+
+		if !needsUpgrade {
+			v.Log.Info("Cilium already updated")
+			return reconciler.Result{}, nil
+		}
+
 		v.Log.Info("About to apply CNI")
 
-		helm := executables.NewHelm(executables.NewExecutable("helm"), executables.WithInsecure())
-		cilium := cilium.NewCilium(nil, helm)
+		helm := executables.NewHelm(executables.NewExecutable("helm"))
+		templater := cilium.NewTemplater(helm)
+		preflight, err := templater.GenerateUpgradePreflightManifest(ctx, specWithBundles)
+		if err != nil {
+			return reconciler.Result{}, err
+		}
+
+		v.Log.Info("Installing Cilium upgrade preflight manifest")
+		if err := reconciler.ReconcileYaml(ctx, remoteClient, preflight); err != nil {
+			return reconciler.Result{}, err
+		}
+		//v.Log.Info("preflight manifest installed", "spec", string(preflight))
+
+		// wait for preflight
+		ciliumDS = &v1.DaemonSet{}
+		ciliumDSName = types.NamespacedName{Namespace: "kube-system", Name: "cilium"}
+		if err := remoteClient.Get(ctx, ciliumDSName, ciliumDS); err != nil {
+			v.Log.V(2).Info("Cilium DS not found")
+			return reconciler.Result{}, err
+		}
+
+		if err := checkDaemonSetReady(ciliumDS); err != nil {
+			v.Log.V(2).Info("Cilium DS not ready")
+			return reconciler.Result{Result: &ctrl.Result{
+				Requeue:      true,
+				RequeueAfter: defaultRequeueTime,
+			}}, nil
+		}
+
+		preFlightCiliumDS := &v1.DaemonSet{}
+		preFlightCiliumDSName := types.NamespacedName{Namespace: "kube-system", Name: "cilium-pre-flight-check"}
+		if err := v.Client.Get(ctx, preFlightCiliumDSName, preFlightCiliumDS); err != nil {
+			v.Log.Info("Preflight Cilium DS not found")
+			return reconciler.Result{}, err
+		}
+
+		if err := checkPreflightDaemonSetReady(ciliumDS, preFlightCiliumDS); err != nil {
+			v.Log.V(2).Info("Preflight DS not ready ")
+			return reconciler.Result{Result: &ctrl.Result{
+				Requeue:      true,
+				RequeueAfter: defaultRequeueTime,
+			}}, nil
+		}
+
+		v.Log.V(2).Info("Deleting Preflight Cilium objects")
+		if err := reconciler.DeleteYaml(ctx, remoteClient, preflight); err != nil {
+			v.Log.V(2).Info("Error deleting Preflight Cilium objects")
+			return reconciler.Result{}, err
+		}
+
+		v.Log.V(3).Info("Generating Cilium upgrade manifest")
+		upgradeManifest, err := templater.GenerateUpgradeManifest(ctx, specWithBundles, specWithBundles)
+		if err != nil {
+			return reconciler.Result{}, err
+		}
+
+		if err := reconciler.ReconcileYaml(ctx, remoteClient, upgradeManifest); err != nil {
+			return reconciler.Result{}, err
+		}
+
+		/*cilium := cilium.NewCilium(nil, helm)
 
 		if err != nil {
 			return reconciler.Result{}, err
@@ -280,14 +381,52 @@ func (v *VSphereClusterReconciler) reconcileCNI(ctx context.Context, cluster *an
 		if err := reconciler.ReconcileYaml(ctx, remoteClient, ciliumSpec); err != nil {
 			return reconciler.Result{}, err
 		}
-		conditions.MarkTrue(cluster, cniSpecAppliedCondition)
+		conditions.MarkTrue(cluster, cniSpecAppliedCondition)*/
 	}
 	return reconciler.Result{}, nil
 }
 
-func (v *VSphereClusterReconciler) reconcileExtraObjects(ctx context.Context, cluster *anywherev1.Cluster, capiCluster *clusterv1.Cluster, specWithBundles *c.Spec) (reconciler.Result, error) {
+// TODO dedup
+func checkPreflightDaemonSetReady(ciliumDaemonSet, preflightDaemonSet *v1.DaemonSet) error {
+	if err := checkDaemonSetObservedGeneration(ciliumDaemonSet); err != nil {
+		return err
+	}
+	if err := checkDaemonSetObservedGeneration(preflightDaemonSet); err != nil {
+		return err
+	}
+
+	if ciliumDaemonSet.Status.NumberReady != preflightDaemonSet.Status.NumberReady {
+		return fmt.Errorf("cilium preflight check DS is not ready: %d want and %d ready", ciliumDaemonSet.Status.NumberReady, preflightDaemonSet.Status.NumberReady)
+	}
+	return nil
+}
+
+// TODO dedup
+func checkDaemonSetReady(daemonSet *v1.DaemonSet) error {
+	if err := checkDaemonSetObservedGeneration(daemonSet); err != nil {
+		return err
+	}
+
+	if daemonSet.Status.DesiredNumberScheduled != daemonSet.Status.NumberReady {
+		return fmt.Errorf("daemonSet %s is not ready: %d/%d ready", daemonSet.Name, daemonSet.Status.NumberReady, daemonSet.Status.DesiredNumberScheduled)
+	}
+	return nil
+}
+
+// TODO dedup
+func checkDaemonSetObservedGeneration(daemonSet *v1.DaemonSet) error {
+	observedGeneration := daemonSet.Status.ObservedGeneration
+	generation := daemonSet.Generation
+	if observedGeneration != generation {
+		return fmt.Errorf("daemonSet %s status needs to be refreshed: observed generation is %d, want %d", daemonSet.Name, observedGeneration, generation)
+	}
+
+	return nil
+}
+
+func (v *VSphereClusterReconciler) reconcileExtraObjects(ctx context.Context, cluster *anywherev1.Cluster, capiCluster *clusterv1.Cluster, specWithBundles *eksacluster.Spec) (reconciler.Result, error) {
 	if !conditions.IsTrue(capiCluster, extraObjectsSpecPlaneAppliedCondition) {
-		extraObjects := c.BuildExtraObjects(specWithBundles)
+		extraObjects := eksacluster.BuildExtraObjects(specWithBundles)
 
 		for _, spec := range extraObjects.Values() {
 			if err := reconciler.ReconcileYaml(ctx, v.Client, spec); err != nil {
@@ -314,7 +453,7 @@ func (v *VSphereClusterReconciler) getCAPICluster(ctx context.Context, cluster *
 
 func (v *VSphereClusterReconciler) reconcileWorkerNodeSpec(
 	ctx context.Context, cluster *anywherev1.Cluster, templateBuilder providers.TemplateBuilder,
-	specWithBundles *c.Spec, workloadTemplateNames, kubeadmconfigTemplateNames map[string]string,
+	specWithBundles *eksacluster.Spec, workloadTemplateNames, kubeadmconfigTemplateNames map[string]string,
 ) (reconciler.Result, error) {
 	if !conditions.IsTrue(cluster, workerNodeSpecPlaneAppliedCondition) {
 		workersSpec, err := templateBuilder.GenerateCAPISpecWorkers(specWithBundles, workloadTemplateNames, kubeadmconfigTemplateNames)
@@ -331,7 +470,7 @@ func (v *VSphereClusterReconciler) reconcileWorkerNodeSpec(
 	return reconciler.Result{}, nil
 }
 
-func (v *VSphereClusterReconciler) reconcileControlPlaneSpec(ctx context.Context, cluster *anywherev1.Cluster, templateBuilder providers.TemplateBuilder, specWithBundles *c.Spec, cpOpt func(values map[string]interface{})) (reconciler.Result, error) {
+func (v *VSphereClusterReconciler) reconcileControlPlaneSpec(ctx context.Context, cluster *anywherev1.Cluster, templateBuilder providers.TemplateBuilder, specWithBundles *eksacluster.Spec, cpOpt func(values map[string]interface{})) (reconciler.Result, error) {
 	if !conditions.IsTrue(cluster, controlSpecPlaneAppliedCondition) {
 		v.Log.Info("Applying control plane spec", "name", cluster.Name)
 		controlPlaneSpec, err := templateBuilder.GenerateCAPISpecControlPlane(specWithBundles, cpOpt)
@@ -346,4 +485,99 @@ func (v *VSphereClusterReconciler) reconcileControlPlaneSpec(ctx context.Context
 		conditions.MarkTrue(cluster, controlSpecPlaneAppliedCondition)
 	}
 	return reconciler.Result{}, nil
+}
+
+func ciliumNeedsUpgrade(ctx context.Context, log logr.Logger, client client.Client, clusterSpec *eksacluster.Spec) (bool, error) {
+	log.Info("Checking if Cilium DS needs upgrade")
+	needsUpgrade, err := ciliumDSNeedsUpgrade(ctx, log, client, clusterSpec)
+	if err != nil {
+		return false, err
+	}
+
+	if needsUpgrade {
+		log.Info("Cilium DS needs upgrade")
+		return true, nil
+	}
+
+	log.Info("Checking if Cilium operator deployment needs upgrade")
+	needsUpgrade, err = ciliumOperatorNeedsUpgrade(ctx, log, client, clusterSpec)
+	if err != nil {
+		return false, err
+	}
+
+	if needsUpgrade {
+		log.Info("Cilium operator deployment needs upgrade")
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func ciliumDSNeedsUpgrade(ctx context.Context, log logr.Logger, client client.Client, clusterSpec *eksacluster.Spec) (bool, error) {
+	ds, err := getCiliumDS(ctx, client)
+	if err != nil {
+		return false, err
+	}
+
+	if ds == nil {
+		log.Info("Cilium DS doesn't exist")
+		return true, nil
+	}
+
+	dsImage := clusterSpec.VersionsBundle.Cilium.Cilium.VersionedImage()
+	containers := make([]corev1.Container, 0, len(ds.Spec.Template.Spec.Containers)+len(ds.Spec.Template.Spec.InitContainers))
+	for _, c := range containers {
+		if c.Image != dsImage {
+			log.Info("Cilium DS container needs upgrade", "container", c.Name)
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func ciliumOperatorNeedsUpgrade(ctx context.Context, log logr.Logger, client client.Client, clusterSpec *eksacluster.Spec) (bool, error) {
+	operator, err := getCiliumDeployment(ctx, client)
+	if err != nil {
+		return false, err
+	}
+
+	if operator == nil {
+		log.Info("Cilium operator deployment doesn't exist")
+		return true, nil
+	}
+
+	operatorImage := clusterSpec.VersionsBundle.Cilium.Operator.VersionedImage()
+	if len(operator.Spec.Template.Spec.Containers) == 0 {
+		return false, errors.New("cilium-operator deployment doesn't have any containers")
+	}
+
+	if operator.Spec.Template.Spec.Containers[0].Image != operatorImage {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func getCiliumDS(ctx context.Context, client client.Client) (*v1.DaemonSet, error) {
+	ds := &v1.DaemonSet{}
+	err := client.Get(ctx, types.NamespacedName{Name: "cilium", Namespace: "kube-system"}, ds)
+	if err != nil {
+		return nil, err
+	}
+
+	return ds, nil
+}
+
+func getCiliumDeployment(ctx context.Context, client client.Client) (*v1.Deployment, error) {
+	deployment := &v1.Deployment{}
+	err := client.Get(ctx, types.NamespacedName{Name: "cilium-operator", Namespace: "kube-system"}, deployment)
+	if apierrors.IsNotFound(err) {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return deployment, nil
 }
--- controllers/controllers/reconciler/reconcile.go
+++ controllers/controllers/reconciler/reconcile.go
@@ -28,12 +28,43 @@ func ReconcileObjects(ctx context.Context, c client.Client, objs []client.Object
 	return nil
 }
 
+func DeleteObjects(ctx context.Context, c client.Client, objs []client.Object) error {
+	for _, o := range objs {
+		if err := DeleteObject(ctx, c, o); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func DeleteObject(ctx context.Context, c client.Client, obj client.Object) error {
+	// Server side apply
+	err := c.Delete(ctx, obj)
+	if err != nil {
+		return errors.Wrapf(err, "failed to delete object %s, %s/%s", obj.GetObjectKind().GroupVersionKind(), obj.GetNamespace(), obj.GetName())
+	}
+
+	return nil
+}
+
 func ReconcileObject(ctx context.Context, c client.Client, obj client.Object) error {
 	// Server side apply
-	err := c.Patch(ctx, obj, client.Apply, &client.PatchOptions{FieldManager: fieldManager})
+	// err := c.Patch(ctx, obj, client.Apply, &client.PatchOptions{FieldManager: fieldManager})
+	t := true
+	err := c.Patch(ctx, obj, client.Apply, &client.PatchOptions{FieldManager: fieldManager, Force: &t})
 	if err != nil {
 		return errors.Wrapf(err, "failed to reconcile object %s, %s/%s", obj.GetObjectKind().GroupVersionKind(), obj.GetNamespace(), obj.GetName())
 	}
 
 	return nil
+}
+
+func DeleteYaml(ctx context.Context, c client.Client, yaml []byte) error {
+	objs, err := YamlToClientObjects(yaml)
+	if err != nil {
+		return err
+	}
+
+	return DeleteObjects(ctx, c, objs)
 }
